ae8783a2a77137f9387fc7e19276453d4bb2d925
Lucas Dias <lucass.diass23@gmail.com>
2026-07-13 22:50:48 -0300

    refactor: correção de responsivo, thumbnails da seção projetos, alinhamento de texto nos cards de contato, e  adição de menu hamburger no layout mobile.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const btnMobile = document.getElementById("btn-mobile");
+const nav = document.getElementById("nav");
+
+btnMobile.addEventListener("click", () => {
+  nav.classList.toggle("active");
+  btnMobile.classList.toggle("active");
+});
